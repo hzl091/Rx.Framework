0f828a07c7984a742883bb39e2745505e12fd3ed
hzl091 <hzl091@126.com>
2017-06-16 18:52:39 +0800

    添加测试

---
+++ Src/Common/Rx.Common.Entities/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿/****************************************************************************************
+ * 文件名：EntityBase
+ * 作者：黄泽林
+ * 创始时间：2017/6/16 9:39:51
+ * 创建说明：
+****************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rx.Common.Entities
+{
+    public abstract class EntityBase
+    {
+    }
+}
---
+++ Test/Rx.Common.Entities.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rx.Common.Entities.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestEntity entity = new TestEntity();
+            entity.Map<TestEntity, string>();
+        }
+    }
+}
